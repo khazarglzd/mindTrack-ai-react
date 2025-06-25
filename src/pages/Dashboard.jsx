76720e7f76a2bf276349b2afff686f1381351477
khazarglzd <khazarguluzade@gmail.com>
2025-06-25 23:55:29 +0400

    feat: create Dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,6 +15,22 @@ const Dashboard = () => {
 
     return (
         <div>
+            <div className="bg-white/80 p-6 rounded-lg shadow-lg text-blue-800 w-full">
+                <h2 className="text-3xl font-bold mb-4">Welcome back 👋</h2>
+                <p className="text-md mb-6">Track your mood or journal your thoughts below.</p>
+
+                <div className="space-y-4">
+                    <textarea
+                        rows="4"
+                        className="w-full p-3 border border-blue-200 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+                        placeholder="How are you feeling today?"
+                    ></textarea>
+
+                    <button className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition">
+                        Save Entry
+                    </button>
+                </div>
+            </div>
             <button
                 onClick={handleLogout}
                 className="bg-white text-blue-600 font-semibold px-6 py-3 rounded shadow hover:bg-gray-100 transition cursor-pointer"
